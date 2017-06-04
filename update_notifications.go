bf551ae29238df10d72c3eb65235599d9f2d72a7
Jhonata Menezes <jhonatamenezes10@gmail.com>
2017-06-04 19:57:20 -0300

    notificacoes sendo enviadas mais de uma vez, identificando problema

--- update_notifications.go
+++ update_notifications.go
@@ -10,6 +10,9 @@ import (
 var scoutsPreparados = map[string]string{"G":"Gol", "CV": "Cartão Vermelho",
 	"CA": "Cartão Amarelo", "GC": "Gol Contra", "DP": "Defesa de Penalti"}
 
+// notificações duplicadas sendos enviadas, mapear as notificações que foram enviadas para nao duplicar
+var atletasNotificados = make(map[int]map[string]int, 1000)
+
 func Run(channelMessage chan *notification.MessageNotification) {
 	time.Sleep(5 * time.Second)
 	CacheLocal := CachePontuados
@@ -17,20 +20,25 @@ func Run(channelMessage chan *notification.MessageNotification) {
 		Pontuados := CachePontuados
 		for atletaId, p := range Pontuados.Atletas {
 			for scout, v := range p.Scout {
+				// apenas os scouts permitidos serão validos
+				if !(len(scoutsPreparados[scout]) > 0) {
+					continue
+				}
+
 				if CacheLocal.Atletas[atletaId].Scout[scout] >= 0 {
 					// scout existe
 					if CacheLocal.Atletas[atletaId].Scout[scout] != v {
-						log.Println("notificando", p.Apelido)
+						log.Println("notificando por scout att", p.Apelido, scout, v)
 						dispatchScout(scout, Pontuados.Atletas[atletaId], channelMessage)
+						CacheLocal = Pontuados
 					}
 				} else {
-					log.Println("notificando", p.Apelido)
+					log.Println("notificando por scout novo", p.Apelido, scout, v)
 					dispatchScout(scout, Pontuados.Atletas[atletaId], channelMessage)
+					CacheLocal = Pontuados
 				}
 			}
 		}
-
-		CacheLocal = Pontuados
 
 		for CacheStatus.StatusMercado != 2 {
 			time.Sleep(60 * time.Minute)
