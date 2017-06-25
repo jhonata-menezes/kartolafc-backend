aef0295b4db644b00ff1fd1aece4d3e0b6065771
Jhonata Menezes <jhonatamenezes10@gmail.com>
2017-06-24 23:26:23 -0300

    suporte a liga mata a mata

--- api/liga.go
+++ api/liga.go
@@ -8,6 +8,19 @@ import (
 
 const URL_LIGA = "/auth/liga/"
 
+type LigaMataMata struct {
+	ChaveId int `json:"chave_id"`
+	LigaId int `json:"liga_id"`
+	TimeMandanteId int `json:"time_mandante_id"`
+	TimeVisitanteId int `json:"time_visitante_id"`
+	VencedorId int `json:"vencedor_id"`
+	RodadaId int `json:"rodada_id"`
+	ChaveSubsequenteId int `json:"chave_subsequente_id"`
+	TipoFase string `json:"tipo_fase"`
+	TimeMandantePontuacao float32 `json:"time_mandante_pontuacao"`
+	TimeVisitantePontuacao float32 `json:"time_visitante_pontuacao"`
+}
+
 type Liga struct {
 	Liga struct{
 		LigaId int `json:"liga_id"`
@@ -22,6 +35,25 @@ type Liga struct {
 		MataMata bool `json:"mata_mata"`
 		Tipo string `json:"tipo"`
 		TotalTimesLiga int `json:"total_times_liga"`
+		// Fields especificas para liga mata a mata
+		InicioRodada int `json:"inicio_rodada"`
+		FimRodada int `json:"fim_rodada"`
+		DataInicio string `json:"data_inicio"`
+		DataFim string `json:"data_fim"`
+		Sorteada bool `json:"sorteada"`
+		TipoFase string `json:"tipo_fase"`
+		UrlTrofeuSvg string `json:"url_trofeu_svg"`
+		Editorial bool `json:"editorial"`
+		Podio []struct{
+			TimeId int `json:"time_id"`
+			Nome string `json:"nome"`
+			NomeCartola string `json:"nome_cartola"`
+			Slug string `json:"slug"`
+			FacebookId int64 `json:"facebook_id"`
+			UrlEscudoSvg string `json:"url_escudo_svg"`
+			FotoPerfil string `json:"foto_perfil"`
+			Assinante bool `json:"assinante"`
+		} `json:"podio"`
 
 	} `json:"liga"`
 
@@ -58,6 +90,7 @@ type Liga struct {
 		} `json:"variacao"`
 
 	} `json:"times"`
+	ChavesMataMata map[string][]LigaMataMata `json:"chaves_mata_mata"`
 }
 
 func (l *Liga) GetLiga(page int) {
