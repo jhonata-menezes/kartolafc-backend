8303be91e0a6b0a2728818053969acfe7248784c
Jhonata Menezes dos Santos <jhonatamenezes10@gmail.com>
2018-04-09 08:54:45 -0300

    query url

--- routes.go
+++ routes.go
@@ -7,7 +7,7 @@ import (
 func BuildRoutes(mux *chi.Mux){
 	mux.Get("/", GetHome)
 	mux.Get("/mercado/status", GetStatus)
-	mux.Get("/times/:q", GetTimes)
+	mux.Get("/times/{q}", GetTimes)
 	mux.Get("/time/id/{id}", GetTime)
 	mux.Get("/time/id/{id}/{rodada}", GetTimeHistorico)
 	mux.Get("/atletas/mercado", GetMercado)
